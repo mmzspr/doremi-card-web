0ce5c993038eea55412e719882d3a2d7e158ff71
0sp <ee197094@meiji.ac.jp>
2023-10-26 04:18:51 +0900

    refactor: フォーマット修正

--- src/types/key.ts
+++ src/types/key.ts
@@ -1,7 +1,15 @@
 export type Scale = 'c' | 'd' | 'e' | 'f' | 'g' | 'a' | 'b';
-export type ItalianScale = "ド" | "レ" | "ミ" | "ファ" | "ソ" | "ラ" | "シ"
+export type ItalianScale = 'ド' | 'レ' | 'ミ' | 'ファ' | 'ソ' | 'ラ' | 'シ';
 export const scaleList: Scale[] = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
-export const italianScaleList: ItalianScale[]  = ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"]
+export const italianScaleList: ItalianScale[] = [
+  'ド',
+  'レ',
+  'ミ',
+  'ファ',
+  'ソ',
+  'ラ',
+  'シ',
+];
 
 export interface IKey {
   scale: Scale;
